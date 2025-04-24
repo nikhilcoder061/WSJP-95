e117a4fd4e257d1e3b697edb70a731d327729820
Nikhil <nikhilcoder061@gmail.com>
2025-04-24 18:26:13 +0530

    Jquery and Slick slider

---
+++ 2 Media query/day3/js/app.js
@@ -0,0 +1,13 @@
+$('.slider').slick(
+    {
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots: true,
+        arrows: true,
+        pauseOnHover: true,
+        speed: 1000,
+    }
+);  
